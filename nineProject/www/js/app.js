28c344be6264511d3ace9f2c9eae4cfa76ee4068
fraski <fraskib@gmail.com>
2016-01-11 20:24:42 +0000

    menu is now semi-working

--- nineProject/www/js/app.js
+++ nineProject/www/js/app.js
@@ -5,88 +5,91 @@
 // the 2nd parameter is an array of 'requires'
 angular.module('starter', ['ionic'])
 
-.config(function($stateProvider, $urlRouterProvider) {
+.config(function ($stateProvider, $urlRouterProvider) {
 
     $stateProvider
+    .state('app', {
+        url: "/app",
+        abstract: true,
+        templateUrl: "app.html"
+    })
     .state('login', {
         url: "/login",
         templateUrl: "login.html"
     })
-    .state('app', {
-      url: "/app",
-      abstract: true,
-      templateUrl: "app.html"
-    })
+
     .state('app.home', {
-      url: "/home",
-      views: {
-        'appContent': {
-          templateUrl: "home.html",
-          controller: "HomeController"
+        url: "/home",
+        views: {
+            'appContent': {
+                templateUrl: "home.html",
+                controller: "HomeController"
+            }
         }
-      }
+    })
+    .state('app.help', {
+        url: "/help",
+        views: {
+            'appContent': {
+                templateUrl: "help.html",
+                controller: "HomeController"
+            }
+        }
     })
 
-  $urlRouterProvider.otherwise("/login");
+    $urlRouterProvider.otherwise("/login");
 })
 
-.run(function($ionicPlatform) {
-  $ionicPlatform.ready(function() {
-    if(window.cordova && window.cordova.plugins.Keyboard) {
-      // Hide the accessory bar by default (remove this to show the accessory bar above the keyboard
-      // for form inputs)
-      cordova.plugins.Keyboard.hideKeyboardAccessoryBar(true);
+.run(function ($ionicPlatform) {
+    $ionicPlatform.ready(function () {
+        if (window.cordova && window.cordova.plugins.Keyboard) {
+            // Hide the accessory bar by default (remove this to show the accessory bar above the keyboard
+            // for form inputs)
+            cordova.plugins.Keyboard.hideKeyboardAccessoryBar(true);
 
-      // Don't remove this line unless you know what you are doing. It stops the viewport
-      // from snapping when text inputs are focused. Ionic handles this internally for
-      // a much nicer keyboard experience.
-      cordova.plugins.Keyboard.disableScroll(true);
-    }
-    if(window.StatusBar) {
-      StatusBar.styleDefault();
-    }
-  });
+            // Don't remove this line unless you know what you are doing. It stops the viewport
+            // from snapping when text inputs are focused. Ionic handles this internally for
+            // a much nicer keyboard experience.
+            cordova.plugins.Keyboard.disableScroll(true);
+        }
+        if (window.StatusBar) {
+            StatusBar.styleDefault();
+        }
+    });
 })
-.controller('AppController', function($scope, $ionicSideMenuDelegate) {
-  $scope.toggleLeft = function() {
-    $ionicSideMenuDelegate.toggleLeft();
-  };
+.controller('AppController', function ($scope, $ionicSideMenuDelegate) {
+    $scope.toggleLeft = function () {
+        $ionicSideMenuDelegate.toggleLeft();
+    };
 })
-.controller("HomeController", function($scope) {
-
-})
-.controller("MenuController", function($scope) {
-
-  $scope.data = {
-    menu_items: []
-  };
-
-  $scope.data.menu_items.push({
-    id: 0,
-    label: "HOME"
-  })
-  $scope.data.menu_items.push({
-    id: 1,
-    label: "MY APPLICATIONS"
-  })
-  $scope.data.menu_items.push({
-    id: 2,
-    label: "HELP"
-  })
-  $scope.data.menu_items.push({
-    id: 3,
-    label: "LOG OUT"
-  })
-  $scope.data.menu_items.push({
-	  id: 4,
-	  label: "CONTACT"
-  })
+.controller("HomeController", function ($scope) {
 
 })
+.controller("MenuController", function ($scope) {
 
-.directive("ionMenu", function() {
-  return {
-    restrict: "E",
-    templateUrl: "ionMenu.html"
-  }
+    $scope.data = {
+        menu_items: []
+    };
+    $scope.menu_items_url = ["app.home", "applications", "app.help", "contact", "login"];
+    $scope.data.menu_items.push({
+        id: 0,
+        label: "HOME"
+    })
+    $scope.data.menu_items.push({
+        id: 1,
+        label: "MY APPLICATIONS"
+    })
+    $scope.data.menu_items.push({
+        id: 2,
+        label: "HELP"
+    })
+    $scope.data.menu_items.push({
+        id: 3,
+        label: "CONTACT"
+    })
+    $scope.data.menu_items.push({
+        id: 4,
+        label: "LOG OUT"
+    })
+
 })
