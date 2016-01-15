b6966613f89f87e88f81ba422e168cff8007fcfd
fraski <fraskib@gmail.com>
2016-01-15 00:05:52 +0000

    BUG EXTERMINATOR MODE; ACTIVATED.

--- nineProject/www/js/ApplicationController.js
+++ nineProject/www/js/ApplicationController.js
@@ -1,10 +1,46 @@
 ﻿//This file includes the appService
+myApp.service('appService', function () {
+    this.selectedApp;
+})
 myApp.controller("ApplicationsController", function ($scope, appService) {
-    $scope.setApp = function(data){
-        $scope.app = data.Apps;
-        console.log(localStorage.getItem("token"));
-        console.log($scope.appNames);
-        console.log($scope.app);
+    $scope.items;
+    $scope.getApps = function () {
+        $(function () {
+            console.log("Before");
+            myURL = "http://3s.alexogden.com:8080/api/apps";
+            $.ajax({
+                url: myURL,
+                type: 'GET',
+                dataType: 'json',
+                success: function (data) { console.log(data); $scope.items = data.Apps; },
+                error: function () { alert("ERROR"); },
+                beforeSend: setHeader,
+                complete: function () {
+                    console.log("COMPLETE");
+                    for (var i = 0; i < $scope.items.length; i++) {
+                        switch (data.Apps[i].os) {
+                            case 0: $scope.items[i].os = "Android"
+                                break;
+                            case 1: $scope.items[i].os = "iOS"
+                                break;
+                            case 2: $scope.items[i].os = "Universal"
+                                break;
+                        }
+                        switch (data.Apps[i].beta) {
+                            case 0: $scope.items[i].beta = null
+                                break;
+                            case 1: $scope.items[i].beta = "BETA"
+                                break;
+                        }
+                    }
+                    $scope.$apply();
+                }
+            });
+        });
+    }
+    $scope.setApp = function (item) {
+        appService.selectedApp = item;
+        console.log(item);
     }
     
     function setHeader(xhr){
@@ -14,51 +50,7 @@ myApp.controller("ApplicationsController", function ($scope, appService) {
         console.log("ASDASDASDASDSAD");
         if (localStorage.getItem("token") != null)
             $scope.getApps();
-       /* else
-            $state.go("app.home");*/
-
-        $scope.getApps = function () {
-            $(function () {
-                console.log("Before");
-                myURL = "http://3s.alexogden.com:8080/api/apps";
-                $.ajax({
-                    url: myURL,
-                    type: 'GET',
-                    dataType: 'json',
-                    success: function(data){ $scope.setApps(data);},
-                    error:function(){alert("ERROR");} ,
-                    beforeSend: setHeader,
-                    complete: function () {
-                        console.log("COMPLETE");
-                        $scope.$apply();
-                        $scope.items = data.Apps;
-
-                        for (var i = 0; i < $scope.items.length; i++) {
-                            switch (data.Apps[i].os) {
-                                case 0: $scope.items[i].os = "Android"
-                                    break;
-                                case 1: $scope.items[i].os = "iOS"
-                                    break;
-                                case 2: $scope.items[i].os = "Universal"
-                                    break;
-                            }
-                            switch (data.Apps[i].beta) {
-                                case 0: $scope.items[i].beta = null
-                                    break;
-                                case 1: $scope.items[i].beta = "BETA"
-                                    break;
-                            }
-                        }
-                        console.log(data);
-                    }
-                });
-            });
-        }
-        $scope.items = [];
-        $scope.getApps();
-
-    })
-    myApp.service('appService',function(){
-        this.selectedApp;
+        /* else
+             $state.go("app.home");*/
     })
 })
